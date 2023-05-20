75c6394e114a953781a844cddc117b147c4467d5
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2023-05-20 11:41:09 +0530

    Add website title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.css'
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <title>Jovi Dex</title>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
